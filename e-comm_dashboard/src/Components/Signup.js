7ee5ea007d020472221f57e5fbf2450011d7697e
Vishwajeet-2000 <Shindevishwajeet007@gmail.com>
2024-09-17 11:24:42 +0530

    Designed the SignUp form

--- e-comm_dashboard/src/Components/Signup.js
+++ e-comm_dashboard/src/Components/Signup.js
@@ -1,11 +1,47 @@
 import React from 'react'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Signup() {
+
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+
+  function collectData(){
+
+  }
+
   return (
-    <div>
-      <h1>Sign up Page</h1>
+    <div className='signup_form'>
+      
+      <Form>
+      <h1 className='mt-5'>Sign up Page</h1>
+        <Form.Group className="mb-3 mt-4">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Email </Form.Label>
+          <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
+          <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-4">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" onClick={collectData} type="submit">Sign Up</Button>
+      </Form>
     </div>
   )
 }
 
 export default Signup;
+
+
+
+
